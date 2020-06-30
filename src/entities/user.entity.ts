74b67e55740eb727287c9bc745671cf1c061c803
Darya Chernyavskaya <dariya.cherniavskaia@nixsolutions.com>
2020-06-30 19:00:01 +0300

    Fixed default location

--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -54,7 +54,7 @@ export class User extends BaseEntity {
   password: string;
 
   @Column({ type: 'point', srid: 4326, nullable: true })
-  location: string;
+  location: any;
 
   @BeforeInsert()
   @BeforeUpdate()
