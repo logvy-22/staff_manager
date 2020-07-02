2a87c72971e5047c1df62c5621c11036556a4119
Ilya Ilyashenko <ilya.iliashenko@nixsolutions.com>
2020-07-02 15:09:30 +0300

    disable socket

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+// import { AppGateway } from './app.gateway';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
 import { VacationsModule } from './vacations/vacations.module';
